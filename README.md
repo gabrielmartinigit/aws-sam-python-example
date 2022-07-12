c17714279bf58eb27320ab4635d067993b1bf34d
Gabriel Martini <gabrielb.martini@gmail.com>
2022-07-12 14:30:30 -0300

    feat(Started microservices) Started noticias and receitas

--- README.md
+++ README.md
@@ -8,9 +8,17 @@
 
 ```bash
     sam init # start sam project
+    sam validate
     sam build
     sam local invoke HelloWorldFunction --event events/hello_world_event.json
-    sam local start-api
+    sam local start-api --env-vars env.json
     sam deploy --stack-name aws-sam-python-example --resolve-s3 --capabilities CAPABILITY_IAM
     sam delete --stack-name aws-sam-python-example
+```
+
+```bash
+    docker run -p 8000:8000 amazon/dynamodb-local
+    aws dynamodb list-tables --endpoint-url http://localhost:8000
+    aws dynamodb create-table --cli-input-json file://data_model/contents_table_model.json --endpoint-url http://localhost:8000
+    aws dynamodb delete-table --table-name ContentsTable --endpoint-url http://localhost:8000
 ```
---
+++ src/noticia/app.py
@@ -0,0 +1,30 @@
+import os
+import json
+import boto3
+
+aws_environment = os.environ['AWSENV']
+table_name = os.environ['TABLE']
+
+# Check if executing locally or on AWS, and configure DynamoDB connection accordingly
+if aws_environment == "AWS_SAM_LOCAL":
+    # SAM LOCAL
+    contents_table = boto3.resource(
+        'dynamodb',
+        endpoint_url="http://127.0.0.1:8000").Table(table_name)
+else:
+    # AWS
+    contents_table = boto3.resource(
+        'dynamodb').Table(table_name)
+
+s3_client = boto3.client("s3")
+
+
+def lambda_handler(event, context):
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "noticia",
+            "env": f"{aws_environment}",
+            "connection": f"{contents_table}"
+        }),
+    }
